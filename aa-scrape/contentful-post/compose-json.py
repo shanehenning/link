dcd7e93bc17beb8a8ec256cb6d803abc938b9a94
Shane Henning <shenning@shenning-mbp.local>
2018-12-05 10:02:54 -0800

    tidying up files

--- aa-scrape/contentful-post/compose-json.py
+++ aa-scrape/contentful-post/compose-json.py
@@ -2,7 +2,6 @@ from openpyxl import load_workbook
 import json
 
 file = load_workbook(filename='advisory-activities-v4-voice-updates-LWP.xlsx')
-activeSheet = file.active
 sheets = file.sheetnames
 
 md = {
@@ -12,12 +11,6 @@ md = {
 }
 allData = []
 data = {}
-default = ''
-def find(string, text):
-    if text in string:
-        return True
-    else:
-        return False
 
 def camelCase(string):
     string = ''.join(x for x in cell.value.title() if not x.isspace())
@@ -29,23 +22,21 @@ for sheet in sheets:
     for row in file[sheet].iter_rows():
         if sheet == 'Service Learning':
             data = {}
+
+            # to limit the amount of activities processed
             if line_count > 3:
                 break
+
             for cellIndex, cell in enumerate(row):
                 if cell.value != None:
                     if line_count == 0:
                         headers.append(camelCase(cell.value))
                     else:
                         data[headers[cellIndex]] = {}
-                        # if headers[cellIndex] in md[sheet]:
-                        #     data[headers[cellIndex]]['en-US'] = {}
-                        #     data[headers[cellIndex]]['en-US']['content'] = []
-                        #     data[headers[cellIndex]]['en-US']['content'].append({"content": [{"value": cell.value}]})
-                        # else:
                         data[headers[cellIndex]]['en-US'] = cell.value
             if line_count > 0:
                 if data != {}:
                     allData.append(data)
             line_count += 1
-        with open('test2.json', 'w') as f:
+        with open('test-output.json', 'w') as f:
             f.write(json.dumps(allData))
